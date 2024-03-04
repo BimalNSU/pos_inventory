ad2704e7b9e542cbf1c0b078d91105125bbb1d63
BimalNSU <bimal.nsu.cse@gmail.com>
2024-03-05 00:15:49 +0600

    feat: update loginController for 3 users

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function redirectTo(){
+        switch(Auth::user()->role){
+            case 1: 
+                $this->redirectTo = '/admin';
+                return $this->redirectTo;
+            case 2:
+                $this->redirectTo = '/manager';
+                return $this->redirectTo;
+            case 3:
+                $this->redirectTo = '/salesman';
+                return $this->redirectTo;
+            default:
+                break;
+        }
     }
 }
