17acad5fee542aadda408136a0ab47cde4bbf51b
BimalNSU <bimal.nsu.cse@gmail.com>
2024-03-10 22:48:00 +0600

    feat: create some models and migration files

---
+++ database/migrations/2024_03_08_115601_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', "50");
+            $table->unsignedInteger('qt');
+            $table->unsignedInteger('price');
+            $table->string('description')->nullable();
+            $table->timestamps();
+            $table->foreign('cat_id')->references('id')->on('categories');
+            // $table->foreign('update_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
